e8504a350f17ded0a5b4033e45ab1aa2f109666c
zzapcho <doyoon201203@gmail.com>
2026-07-05 17:32:04 +0900

    load minimal console polish

--- console/src/main.tsx
+++ console/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
+import "./polish.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
