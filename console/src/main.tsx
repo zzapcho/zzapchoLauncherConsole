1b3bfc7293f5f95b1971e46e8f651cba5638b0b0
zzapcho <doyoon201203@gmail.com>
2026-07-05 22:10:58 +0900

    feat: load modal styles

--- console/src/main.tsx
+++ console/src/main.tsx
@@ -4,5 +4,6 @@ import { App } from "./App";
 import "./index.css";
 import "./launcherContent.css";
 import "./homeFlow.css";
+import "./modal.css";
 
 createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
