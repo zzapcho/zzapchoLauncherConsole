96504a91dd53c14b47546fb3e19a0df479f95ac2
zzapcho <doyoon201203@gmail.com>
2026-07-05 19:09:58 +0900

    feat: use redesigned console app

--- console/src/main.tsx
+++ console/src/main.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { LauncherStyleConsole } from "./LauncherStyleConsole";
-import "./styles.css";
-import "./polish.css";
-import "./launcherStyleConsole.css";
-import "./editorLarge.css";
-import "./toggle.css";
+import { App } from "./App";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <LauncherStyleConsole />
+    <App />
   </React.StrictMode>,
 );
