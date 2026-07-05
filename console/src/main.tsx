e128571361dd62d72222762fc2268e6ae801c17b
zzapcho <doyoon201203@gmail.com>
2026-07-05 22:06:17 +0900

    feat: load home flow css

--- console/src/main.tsx
+++ console/src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
 import "./launcherContent.css";
+import "./homeFlow.css";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
