5fb80ecd033f8d6b70df5d175aba49c22b59ec7c
zzapcho <doyoon201203@gmail.com>
2026-07-05 23:07:02 +0900

    feat: enable PWA registration and mobile styles

--- console/src/main.tsx
+++ console/src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { registerPwa } from "./pwa";
 import "./index.css";
 import "./launcherContent.css";
 import "./homeFlow.css";
 import "./modal.css";
 import "./editorRefine.css";
 import "./assetLayoutFix.css";
+import "./mobile.css";
+
+registerPwa();
 
 const root = document.getElementById("root");
 if (!root) throw new Error("root element not found");
