fa3cfed88f17ded5e4345b15a6be7c86eb75c141
zzapcho <doyoon201203@gmail.com>
2026-07-07 08:19:04 +0900

    feat: load mobile polish layer

--- console/src/App.tsx
+++ console/src/App.tsx
@@ -1,6 +1,7 @@
 import { ConsoleAppFresh } from "./ConsoleAppFresh";
 import "./freshConsole.css";
 import "./freshConsoleFix.css";
+import "./freshMobile.css";
 
 export function App() {
   return <ConsoleAppFresh />;
