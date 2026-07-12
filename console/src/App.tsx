8c54507d21963169e4daa1f07c3078e8783f4fce
zzapcho <doyoon201203@gmail.com>
2026-07-12 09:04:08 +0900

    feat: register asset version UI enhancer

--- console/src/App.tsx
+++ console/src/App.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import { ConsoleAppFresh } from "./ConsoleAppFresh";
+import { registerFreshAssetVersionEnhancer } from "./freshAssetVersionEnhancer";
 import "./freshConsole.css";
 import "./freshConsoleFix.css";
 import "./freshMobile.css";
 import "./freshToggleFix.css";
+import "./freshAssetVersion.css";
 
 export function App() {
+  useEffect(() => {
+    registerFreshAssetVersionEnhancer();
+  }, []);
+
   return <ConsoleAppFresh />;
 }
