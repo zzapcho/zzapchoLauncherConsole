f501c70b0d4d0fa2bf1cfdfa0057631b4237a537
zzapcho <doyoon201203@gmail.com>
2026-07-12 09:05:49 +0900

    feat: load CurseForge support metadata cache

--- console/src/App.tsx
+++ console/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { ConsoleAppFresh } from "./ConsoleAppFresh";
 import { registerFreshAssetVersionEnhancer } from "./freshAssetVersionEnhancer";
+import "./curseForgeVersionCache";
 import "./freshConsole.css";
 import "./freshConsoleFix.css";
 import "./freshMobile.css";
