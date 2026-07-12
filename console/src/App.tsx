6cd1f1334f217603954c4231f60abb765f069e03
zzapcho <doyoon201203@gmail.com>
2026-07-12 09:38:09 +0900

    fix: register floating mobile dock

--- console/src/App.tsx
+++ console/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { ConsoleAppFresh } from "./ConsoleAppFresh";
 import { registerFreshAssetVersionEnhancer } from "./freshAssetVersionEnhancer";
+import { registerFreshMobileDock } from "./freshMobileDock";
 import "./curseForgeVersionCache";
 import "./freshConsole.css";
 import "./freshConsoleFix.css";
@@ -8,10 +9,12 @@ import "./freshMobile.css";
 import "./freshToggleFix.css";
 import "./freshAssetVersion.css";
 import "./freshMobileFinal.css";
+import "./freshMobileDock.css";
 
 export function App() {
   useEffect(() => {
     registerFreshAssetVersionEnhancer();
+    registerFreshMobileDock();
   }, []);
 
   return <ConsoleAppFresh />;
